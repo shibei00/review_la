9e4b4f3f1dc88d656b3665af54cf1bf249dc6c95
shibei <shibei00@gmail.com>
2015-01-12 11:44:05 +0800

    a

--- author.py
+++ author.py
@@ -6,13 +6,24 @@ if __name__=='__main__':
     f = open('/misc/projdata4/info_fil/bshi/Data/review/bing_liu/amazon-member-shortSummary.txt', 'r')
     line_list = f.readlines()
     f.close()
+    conn = MySQLdb.connect(host='seis10', user='bshi', passwd='20141031shib', db='bshi', charset='utf-8')
+    
     for i in xrange(0, len(line_list)):
         if line_list[i].strip()=='MEMBER INFO':
             info_line=line_list[i+1]
             t_info_list = [x.strip() for x in info_line.split('\t')]
             try:
                 member_id = t_info_list[0]
+                member_name = t_info_list[1]
                 review_number = int(t_info_list[2])
-                print member_id, review_number                
+                if review_number >= 20:
+                    cur = conn.cursor()
+                    try:
+                        i_sql = 'insert into member_info(member_id, member_name, review_number) values (%s, %s, %s)'
+                        cur.execute(i_sql, (member_id, member_name, review_number))
+                        conn.commit()
+                    except:
+                        conn.rollback()
             except:
                 traceback.print_exc()
+    conn.close()
