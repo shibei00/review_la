c5d4aaab3723f663fd41f4c3ca6b0c51528e2eac
shibei <shibei00@gmail.com>
2015-01-12 11:16:48 +0800

    a

--- author.py
+++ author.py
@@ -5,9 +5,8 @@ if __name__=='__main__':
     f = open('/misc/projdata4/info_fil/bshi/Data/review/bing_liu/amazon-member-shortSummary.txt', 'r')
     line_list = f.readlines()
     f.close()
-    print line_list
     for i in xrange(0, len(line_list)):
-        if line_list[i]=='MEMBER INFO':
+        if line_list[i].strip()=='MEMBER INFO':
             info_line=line_list[i+1]
             t_info_list = [x.strip() for x in info_line.split('\t')]
             member_id = t_info_list[0]
