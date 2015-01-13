4941c50b78eb76c0f4d96ddab326b60a319a23ac
shibei <shibei00@gmail.com>
2015-01-13 11:50:20 +0800

    a

--- review_incomplete.py
+++ review_incomplete.py
@@ -2,26 +2,56 @@ import MySQLdb
 import traceback
 
 if __name__=='__main__':
-    f = open('result_list.txt')
-    line_list = f.readlines()
-    f.close()
-    conn = MySQLdb.connect(host='seis10', user='bshi', passwd='20141031shib', db='bshi', charset='utf8')    
-    for line in line_list:
-        info_list = [x.strip() for x in line.split()]
-        member_id = info_list[0]
-        cur = conn.cursor()
-        sql = 'select * from review_info where member_id=%s'
-        try:
-            cur.execute(sql, member_id)
+    seedproduct = ['0895260174',]
+    seedmember = []
+    result_row = {}
+    
+    conn = MySQLdb.connect(host='seis10', user='bshi', passwd='20141031shib', db='bshi', charset='utf8')
+    sql = 'select * from review_info where member_id=%s'
+    sql2 = 'select * from review_info where product_id=%s'
+
+    count = 0
+
+    while(len(result_row.keys()) < 20000):
+        for p in seedproduct:
+            cur = conn.cursor()
+            cur.execute(sql2, p)
             rows = cur.fetchall()
-            cur.close()
             for row in rows:
+                member_id = row[1]
                 cur2 = conn.cursor()
-                sql2 = 'insert into review_info values(%s, %s, %s, %s, %s, %s)'
+                cur2.execute(sql, member_id)
+                rows_member = cur2.fetchall()
+                if len(rows_member) > 10 and len(rows_member) < 100:
+                    result_row[row] = 1
+                    seedmember.append(member_id)
+        seedproduct = []
+        if len(result_row.keys()) > 20000:
+            break
+            
+        for p in seedmember:
+            cur = conn.cursor()
+            cur.execute(sql, p)
+            rows = cur.fetchall()
+            for row in rows():
+                product_id = row[2]
+                cur2 = conn.cursor()
+                cur2.execute(sql2, product_id)
+                rows_product = cur2.fetchall()
+                if len(rows_product) > 10 and len(rows_product) < 100:
+                    result_row[row] = 1
+                    seedproduct.append(product_id)
+        seedmember = []
+
+
+        try:
+            for row in result_row.keys():
+                cur2 = conn.cursor()
+                sql2 = 'insert into review_info_incomplete(member_id, product_id, date, rating, title, body) values(%s, %s, %s, %s, %s, %s)'
                 cur2.execute(sql2, row[1], row[2], row[3], row[4], row[5], row[6])
-                cur.close()
         except:
             conn.rollback()
+            traceback.print_exc()
     conn.commit()
     conn.close()
 
