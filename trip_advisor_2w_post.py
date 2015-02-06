9ea334ffe7a0d29ffeef23ec4390acacab89778e
shibei <shibei00@gmail.com>
2015-02-06 21:39:26 +0800

    a

--- trip_advisor_2w_post.py
+++ trip_advisor_2w_post.py
@@ -13,15 +13,16 @@ if __name__=='__main__':
     conn = MySQLdb.connect(host='seis10', user='bshi', passwd='20141031shib', db='bshi', charset='utf8')
     f_name = '/misc/projdata4/info_fil/bshi/Code/review_lda/trip_advisor_2w_raw_new.txt'
     f_lines = read_file(f_name)
-    sql = 'update trip_advisor_2w_info set is_exist=1 where member_id=%s and product_id=%s and rating=%s'
+    sql = 'update trip_advisor_2w_info set is_exist=1 where member_id=%s and product_id=%s and rating=%s and helpful_score=%s'
     for line in f_lines:
         t_list = line.split('\t')
         if len(t_list) > 1:
             member_id = t_list[0]
             product_id = t_list[1]
             rating = t_list[2]
+            help_score = t_list[3]
             cur = conn.cursor()
-            cur.execute(sql, (member_id, product_id, rating))
+            cur.execute(sql, (member_id, product_id, rating, help_score))
             conn.commit()
 
     sql2 = 'delete from trip_advisor_2w_info where is_exist=0'
