bffa9463c45e44d3aa3643b65d98183baa8e4977
shibei <shibei00@gmail.com>
2015-02-07 14:30:07 +0800

    a

--- base_line.py
+++ base_line.py
@@ -337,6 +337,7 @@ if __name__=='__main__':
                         r_spam_is_RA_count -= 1
                 first_factor_non= (n_m_non_dict[j] + 1) / float(n_m_count[j] -1 + 2)
                 first_factor_spam = (n_m_spam_dict[j] + 1) / float(n_m_count[j] - 1 + 2)
+                print first_factor_non, first_factor_spam
                 sf_DUP_non=0.0
                 if r_DUP_dict[k]==1:
                     sf_DUP_non = (r_non_is_DUP_count + 1) / float(non_total_count + 2)
